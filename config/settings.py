ceb96e126143826e9a9e0ab6ae68d8de7ab6a701
asadtop4ik <asadbek.backend@gmail.com>
2024-06-20 12:09:14 +0500

    new

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-0s45)9v+s(42@xs-=x8d12(u*1kri)9t#htqb&v4u^)e5t()h0'
